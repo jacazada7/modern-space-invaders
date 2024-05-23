51acbef138c5f97249ae7f943df2924819fae7f6
jacazada7 <joaoalvves028@gmail.com>
2024-05-23 11:30:26 -0400

    add draw function in PowerUp class

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -65,7 +65,15 @@ class PowerUp {
     constructor({position, velocity,}) {
         this.position = position;
         this.velocity = velocity;
-        this.radius = 15;
+        this.radius = 15; 
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.fillStyle = "yellow";
+        c.fill(); 
+        c.closePath();
         
     }
 }

cc4cca1f7e00aa8889e800915768e364a67aef3e
jacazada7 <joaoalvves028@gmail.com>
2024-05-23 11:32:54 -0400

    add update function in powerup class

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -73,7 +73,11 @@ class PowerUp {
         c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
         c.fillStyle = "yellow";
         c.fill(); 
-        c.closePath();
-        
+        c.closePath(); 
     }
+
+    update() {
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        this.draw();
 }

6f77358ddb183baded57f63e857bcb1873172889
jacazada7 <joaoalvves028@gmail.com>
2024-05-28 11:12:59 -0400

    add InavaderProjectile class

---
+++ js/classes/InvaderProjectile.js
@@ -0,0 +1,9 @@
+class invaderProjectiles {
+    constructor({position, velocity}) {
+        this.position = position;
+        this.velocity = velocity;
+        this.width = 3;
+        this.height = 10;
+  
+    }
+}

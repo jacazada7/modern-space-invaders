468b27f2630dcbf885185d85caecd1313592c71b
jacazada7 <joaoalvves028@gmail.com>
2024-05-28 11:30:18 -0400

    add update function in Projectile class

--- js/classes/Projectile.js
+++ js/classes/Projectile.js
@@ -13,4 +13,11 @@ class Projectile {
         c.fill();
         c.closePath();
     }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        
+    }
 }

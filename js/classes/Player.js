4fdbc002d4082ebd1e5eda97a2cae3d00e9844eb
jacazada7 <joaoalvves028@gmail.com>
2024-06-06 10:25:24 -0400

    add draw function in Player class

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -21,6 +21,29 @@ class Player {
             this.particles = [];
             this.frames = 0;
         };
+}
+draw() {
+    c.save();
+    c.globalAlpha = this.opacity;                                                                                                                                                                                                                   
+    c.translate(
+     player.position.x + player.width / 2,
+     player.position.y + player.height / 2
+    );
+    c.rotate(this.rotation);
+    
+    c.translate(
+    -player.position.x - player.width / 2,
+    -player.position.y - player.height / 2
+    );
 
+    c.drawImage(
+        this.image,
+        this.position.x,
+        this.position.y,
+        this.width,
+        this.height
+    );
+
+    c.restore();
 }
 }

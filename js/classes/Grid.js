94de709337b7bd9c939799a43f79676064149467
jacazada7 <joaoalvves028@gmail.com>
2024-06-11 11:19:01 -0400

    add standardized formatting of project files

--- js/classes/Grid.js
+++ js/classes/Grid.js
@@ -33,10 +33,9 @@ class Grid {
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
     this.velocity.y = 0;
-    if(this.position.x + this.width >= canvas.width || this.position.x <0) {
+    if (this.position.x + this.width >= canvas.width || this.position.x < 0) {
       this.velocity.x = -this.velocity.X * 1.15;
       this.velocity.y = 30;
-
     }
   }
 }

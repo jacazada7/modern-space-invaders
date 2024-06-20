7dc54638eb5378e992bb29d66ce1018b026e1e95
jacazada7 <joaoalvves028@gmail.com>
2024-06-20 08:39:02 -0400

    fixed projectile.js

--- js/classes/Projectile.js
+++ js/classes/Projectile.js
@@ -8,7 +8,7 @@ class Projectile {
 
   draw() {
     c.beginPath();
-    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
     c.fillStyle = this.color;
     c.fill();
     c.closePath();

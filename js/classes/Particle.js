c77c368c4ba6d6f82138b9ce7a091569e336dabe
jacazada7 <joaoalvves028@gmail.com>
2024-06-20 08:40:57 -0400

    fixed particle.js

--- js/classes/Particle.js
+++ js/classes/Particle.js
@@ -1,23 +1,27 @@
+
 class Particle {
   constructor({ position, velocity, radius, color, fades }) {
     this.position = position;
     this.velocity = velocity;
     this.radius = radius;
+
     this.color = color;
-    this.fades = fades;
+
     this.opacity = 1;
+    this.fades = fades;
   }
 
   draw() {
     c.save();
     c.globalAlpha = this.opacity;
-    c.fillStyle = this.color;
     c.beginPath();
     c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = this.color;
     c.fill();
     c.closePath();
     c.restore();
   }
+
   update() {
     this.draw();
     this.position.x += this.velocity.x;
